0504ac4ee55da2a974786e03c1aa3f1a5a67cf70
Seemaya06 <71091580+Seemaya06@users.noreply.github.com>
2021-08-28 19:16:31 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,18 @@
+
+var box;
+
+
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background("black");
+
+  rect(200,200,50,50)
+
+ /* box = createSprite(200,200,50,50);
+  box.shapeColor = "white";
+
+  drawSprites();*/
+}
